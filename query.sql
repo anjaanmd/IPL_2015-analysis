1cf20644cc70f3e19f617a616ce2bd0ae3900cd8
anjaanmd <mohandoss.ez@gmail.com>
2016-10-23 18:50:53 +0530

    Modified query

--- query.sql
+++ query.sql
@@ -1,7 +1,6 @@
 USE ipl_2015;
 
 
-
 SELECT 
         count(match_id) AS `Innings`,
         sum(R) AS `Runs`,
@@ -238,3 +237,18 @@ GROUP BY batsman,
          team
 ORDER BY `Runs` DESC LIMIT 25;
 
+
+ 
+SELECT count(*) AS `Innings`,
+       sum(R) AS `Runs`,
+       sum(B) AS `Balls`,
+       sum(dismissal LIKE 'not%') AS 'Not out',
+       max(R) AS `Highest`,
+       cast((sum(R) / (count() - sum(dismissal LIKE 'not%'))) AS DECIMAL(5,2)) `Average`,
+       cast((sum(R) * 100 / sum(B)) AS DECIMAL(5,2)) `SR`,
+       sum(4s) AS `4s`,
+       sum(6s) AS `6s`,
+       sum(M) AS `Minutes`,
+       team AS `Team`
+FROM ipl_batting
+ORDER BY `Runs` DESC LIMIT 25;
