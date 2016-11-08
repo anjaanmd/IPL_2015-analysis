75a816873e43c428de93a53e501c53b98b9e6c3d
anjaanmd <mohandoss.ez@gmail.com>
2016-11-09 00:52:49 +0530

    Added ggplot_theme file and markdown file

---
+++ my_theme.R
@@ -0,0 +1,26 @@
+theme_mine <- function (base_size = 12, color = "brown", base_family = "helvetica", 
+    title_family = "mono") 
+{
+    colorhex <- ggthemes_data$wsj$bg[color]
+    (theme_foundation(base_size = base_size, base_family = base_family) + 
+        theme(line = element_line(linetype = 1, colour = "black"), 
+            rect = element_rect(fill = colorhex, linetype = 0, 
+                colour = NA), text = element_text(colour = "black"), 
+            title = element_text(family = base_family, size = rel(1.5)), 
+            axis.title = element_text(face = "bold",size = rel(.75)),
+            axis.text = element_text(face = "bold", 
+                size = rel(1)), axis.text.x = element_text(colour = NULL),               
+          # axis.title.y = element_text(angle=90,vjust =2),
+        #             axis.title.x = element_text(vjust = -0.2),
+            axis.text.y = element_text(colour = NULL), axis.ticks = element_line(colour = NULL), 
+            axis.ticks.y = element_blank(), axis.ticks.x = element_line(colour = NULL), 
+            axis.line = element_line(), axis.line.y = element_blank(), 
+            legend.background = element_rect(), legend.position = "right", 
+ #           legend.direction = "horizontal", legend.box = "vertical", 
+            legend.direction = "vertical", legend.box = "vertical",
+            panel.grid = element_line(colour = NULL, linetype = 0), 
+            panel.grid.major = element_line(colour = "black"), 
+            panel.grid.major.x = element_blank(), panel.grid.minor = element_blank(), 
+            plot.title = element_text(hjust = 0, face = "bold"), 
+            plot.margin = unit(c(10,5,5,5),"mm"), strip.background = element_rect()))
+}
