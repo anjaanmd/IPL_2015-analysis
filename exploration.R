fb10ea1dcd3da329a22ec1e5c95ea56e113be5d0
anjaanmd <mohandoss.ez@gmail.com>
2016-11-10 20:31:31 +0530

    overall_extras_comb_pie and overall_extras_and_batsmen_pie finished

--- exploration.R
+++ exploration.R
@@ -8,7 +8,7 @@ overall_runs_by_team_bar <- ggplot(Overall_runs_by_teams_ipl,
   geom_bar(position = "dodge", stat = "identity") + ggtitle("Total Runs scored by the Teams") + xlab("") + ylab("Runs") +
   coord_flip() + theme_mine()
 
-ggplotly(overall_runs_by_team_gg, tooltip = c("text", "y", "fill"))
+#ggplotly(overall_runs_by_team_gg, tooltip = c("text", "y", "fill"))
 
 overall_runs_by_team_pie <-
   Overall_runs_by_teams_ipl %>% filter(Result == "All") %>%
@@ -51,37 +51,42 @@ overall_runs_distribution_all_pie <-
          aes(x = Result, y = Runs, fill = Runs_class)) +
   geom_bar(stat = "identity", width = 1) + labs(title = "All") +
   theme_mine_no_legend() + coord_polar(theta = "y")  +
-  geom_text(aes(y = Runs / 2 + c(0, cumsum(Runs)[-length(Runs)]),
-                label = percent(Runs / sum(Runs)))) +
-  theme(plot.title = element_text(size = 15, margin = margin(b = -5, unit = "pt")))
+  geom_text(aes(x= 1.2, y = Runs / 2 + c(0, cumsum(Runs)[-length(Runs)]),
+                label = round(Runs * 100 / sum(Runs), digits = 1))) + 
+  theme(plot.title = element_text(size = 13, face = "bold", margin = margin(b = -5, unit = "pt")))
 
 overall_runs_distribution_won_pie <-
   ggplot(overall_runs_distribution_won,
          aes(x = Result, y = Runs, fill = Runs_class)) +
   geom_bar(stat = "identity", width = 1) + labs(title = "Won") +
   theme_mine_no_legend() + coord_polar(theta = "y")  +
-  geom_text(aes(y = Runs / 2 + c(0, cumsum(Runs)[-length(Runs)]),
-                label = percent(Runs / sum(Runs)))) +
-  theme(plot.title = element_text(size = 15, margin = margin(b = -5, unit = "pt")))
+  geom_text(aes(x= 1.2, y = Runs / 2 + c(0, cumsum(Runs)[-length(Runs)]),
+                label = round(Runs * 100 / sum(Runs), digits = 1))) +
+  theme(plot.title = element_text(size = 13, face = "bold", margin = margin(b = -5, unit = "pt")))
 
 overall_runs_distribution_lost_pie <-
   ggplot(overall_runs_distribution_lost,
          aes(x = Result, y = Runs, fill = Runs_class)) +
   geom_bar(stat = "identity", width = 1) + labs(title = "Lost") +
   theme_mine_no_legend() + coord_polar(theta = "y")  +
-  geom_text(aes(y = Runs / 2 + c(0, cumsum(Runs)[-length(Runs)]),
-                label = percent(Runs / sum(Runs)))) + 
-  theme(plot.title = element_text(size = 15, margin = margin(b = -5, unit = "pt")))
+  geom_text(aes(x= 1.2, y = Runs / 2 + c(0, cumsum(Runs)[-length(Runs)]),
+                label = round(Runs * 100 / sum(Runs), digits = 1))) + 
+  theme(plot.title = element_text(size = 13, face = "bold", margin = margin(b = -5, unit = "pt")))
 
+overall_extras_and_batsmen_comb_pie <-
+  grid.arrange(overall_runs_distribution_all_pie, overall_runs_distribution_won_pie, 
+               overall_runs_distribution_lost_pie, 
+               top = textGrob("Runs scored by the Teams",
+                              gp=gpar(fontsize=18,font_family = "helvetica", fontface = "bold"), vjust=4.25), 
+               bottom = textGrob("Composition of runs by batsmen and extras(in %)", 
+                                 gp=gpar(fontsize=15,font_family = "helvetica"), vjust=-17.65), 
+               nrow = 1, ncol = 3)
 
+legend_extras_batsmen <- g_legend(ggplot(overall_runs_distribution_lost,
+	aes(x = Result, y = Runs, fill = Runs_class)) +
+	geom_bar(stat = "identity", width = 1) + labs(title = "All") +
+	theme_mine_legend_bottom())
 
-
-
-
-ggplot(overall_runs_distribution,
-       aes(x = Result, y = Runs, fill = Runs_class)) +
-  geom_bar(stat = "identity", width = 1) + ggtitle("Total Runs scored by the Teams") + xlab("") + ylab("Runs") +
-  coord_flip() + theme_mine()
 
 overall_extras_all <-
   gather(Overall_runs_ipl,
@@ -89,13 +94,6 @@ overall_extras_all <-
          value = `Runs`,-c(Result, Team, Matches)) %>%
   filter(!Extras_class %in% c("Runs", "Runs by Batsmen", "Extras"),
          Result == "All")
-
-ggplot(overall_extras_all,
-       aes(x = Result, y = Runs, fill = Extras_class)) +
-  geom_bar(stat = "identity", width = 1) + ggtitle("Total Runs scored by the Teams") + xlab("") + ylab("Runs") +
-  theme_mine() + coord_polar(theta = "y") + theme(axis.text.x = element_blank()) +
-  geom_text(aes(y = Runs / 2 + c(0, cumsum(Runs)[-length(Runs)]),
-                label = percent(Runs / sum(Runs))))
 
 overall_extras_won <-
   gather(Overall_runs_ipl,
@@ -114,16 +112,41 @@ overall_extras_lost <-
 
 
 
-ggplot(overall_extras_won,
+overall_extras_all_pie <- ggplot(overall_extras_all,
        aes(x = Result, y = Runs, fill = Extras_class)) +
-  geom_bar(stat = "identity", width = 1) + ggtitle("Total Runs scored by the Teams") + xlab("") + ylab("Runs") +
-  theme_mine() + coord_polar(theta = "y") + theme(axis.text.x = element_blank()) +
-  geom_text(aes(y = Runs / 2 + c(0, cumsum(Runs)[-length(Runs)]),
-                label = percent(Runs / sum(Runs))))
+  geom_bar(stat = "identity", width = 1) + labs(title = "All") +
+  theme_mine_no_legend() + coord_polar(theta = "y")  +
+  geom_text(aes(x= 1.2, y = Runs / 2 + c(0, cumsum(Runs)[-length(Runs)]),
+                label = round(Runs * 100 / sum(Runs), digits = 1))) + 
+  theme(plot.title = element_text(size = 13, face = "bold", margin = margin(b = -5, unit = "pt")))
 
-ggplot(overall_extras_lost,
+overall_extras_won_pie <- ggplot(overall_extras_won,
        aes(x = Result, y = Runs, fill = Extras_class)) +
-  geom_bar(stat = "identity", width = 1) + ggtitle("Total Runs scored by the Teams") + xlab("") + ylab("Runs") +
-  theme_mine() + coord_polar(theta = "y") + theme(axis.text.x = element_blank()) +
-  geom_text(aes(y = Runs / 2 + c(0, cumsum(Runs)[-length(Runs)]),
-                label = percent(Runs / sum(Runs))))
+  geom_bar(stat = "identity", width = 1) + labs(title = "Won") +
+  theme_mine_no_legend() + coord_polar(theta = "y")  +
+  geom_text(aes(x= 1.2, y = Runs / 2 + c(0, cumsum(Runs)[-length(Runs)]),
+                label = round(Runs * 100 / sum(Runs), digits = 1))) + 
+  theme(plot.title = element_text(size = 13, face = "bold", margin = margin(b = -5, unit = "pt")))
+
+overall_extras_lost_pie <- ggplot(overall_extras_lost,
+       aes(x = Result, y = Runs, fill = Extras_class)) +
+  geom_bar(stat = "identity", width = 1) + labs(title = "Lost") +
+  theme_mine_no_legend() + coord_polar(theta = "y")  +
+  geom_text(aes(x= 1.2, y = Runs / 2 + c(0, cumsum(Runs)[-length(Runs)]),
+                label = round(Runs * 100 / sum(Runs), digits = 1))) + 
+  theme(plot.title = element_text(size = 13, face = "bold",  margin = margin(b = -5, unit = "pt")))
+
+
+overall_extras_comb_pie <-
+		grid.arrange(overall_extras_all_pie, overall_extras_won_pie, 
+			overall_extras_lost_pie, top = textGrob("Extras received by the Teams",
+				gp=gpar(fontsize=18,font_family = "helvetica", fontface = "bold"), vjust=4.25), 
+			bottom = textGrob("Composition of extras by category(in %)", 
+			                  gp=gpar(fontsize=15,font_family = "helvetica"), vjust=-17.65),  
+			nrow = 1, ncol = 3)
+
+
+legend_extras <- g_legend(ggplot(overall_extras_lost,
+       aes(x = Result, y = Runs, fill = Extras_class)) +
+  geom_bar(stat = "identity", width = 1) +
+	theme_mine_legend_bottom())
