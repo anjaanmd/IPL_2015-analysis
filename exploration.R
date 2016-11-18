c595c0a8a8e88af95b7d9c60a60aa765fbc0e2fe
anjaanmd <mohandoss.ez@gmail.com>
2016-11-18 20:44:19 +0530

    added overall_runs_by_teams bar and pie using theme_new in exploration.r

--- exploration.R
+++ exploration.R
@@ -10,9 +10,6 @@ library(ggiraph)
 library(ggvis)
 library(gganimate)
 library(animation)
-
-
-
 overall_runs_by_team_bar <- ggplot(Overall_runs_by_teams_ipl,
                                    aes(
                                      x = reorder(Team, -Runs),
@@ -20,20 +17,32 @@ overall_runs_by_team_bar <- ggplot(Overall_runs_by_teams_ipl,
                                      fill = Result,
                                      text = paste("Team: ", reorder(Team, -Runs))
                                    )) +
-  geom_bar(position = "dodge", stat = "identity") + ggtitle("Total Runs scored by the Teams") + xlab("") + ylab("Runs") +
-  coord_flip() + theme_mine_pie()
+  geom_bar(position = "dodge", stat = "identity") + ggtitle("Total Runs scored by the Teams\n") + xlab("") + ylab("Runs") +
+  coord_flip() + theme_new() +
+  scale_fill_manual(values = wes_palette("FantasticFox"))+
+  theme(plot.title=element_text(size=rel(1.5), hjust=0.25,
+                                face = "bold",
+                                colour = "#000c52"))
+
+ggsave("graphs/overall_runs_by_team_bar.svg", overall_runs_by_team_bar, 
+       width = 13, height = 8) 
 
 #ggplotly(overall_runs_by_team_gg, tooltip = c("text", "y", "fill"))
 
-overall_runs_by_team_pie <-
-  Overall_runs_by_teams_ipl %>% filter(Result == "All") %>%
+overall_runs_by_team_pie <- Overall_runs_by_teams_ipl %>% filter(Result == "All") %>%
   ggplot(., aes(x = Result, y = Runs, fill = Team)) +
   geom_bar(stat = "identity") + ggtitle("Runs scored by the Teams") +
-  xlab("") + ylab("Runs") +
-  theme_mine() + coord_polar(theta = "y") + theme(axis.text.x =
-                                                    element_blank()) +
-  geom_text(aes(y = Runs / 2 + c(0, cumsum(Runs)[-length(Runs)]),
-                label = percent(Runs / sum(Runs))))
+  theme_new() + coord_polar(theta = "y", direction = -1) + 
+  theme(axis.text.x = element_blank(), axis.text.y = element_blank(),
+        legend.position = "right", 
+        legend.direction = "vertical", legend.key.size = unit(2, 'lines') , axis.title = element_blank()) +
+  geom_text(aes(x=1.2, y = Runs/4  + c(0, cumsum(Runs)[-length(Runs)]),
+                label = round(Runs * 100 / sum(Runs), digits = 1)), 
+            size = 7) + 
+  scale_fill_economist()
+
+ggsave("graphs/overall_runs_by_team_pie.svg", overall_runs_by_team_pie,
+       width = 13, height = 8) 
 
 # overall_runs_distribution <- gather(Overall_runs_ipl, key = `Runs_class`,
 # 								value = `Runs`, -c(Result, Team, Matches)) %>%
@@ -98,9 +107,9 @@ overall_extras_and_batsmen_comb_pie <-
                nrow = 1, ncol = 3)
 
 legend_extras_batsmen <- g_legend(ggplot(overall_runs_distribution_lost,
-	aes(x = Result, y = Runs, fill = Runs_class)) +
-	geom_bar(stat = "identity", width = 1) + labs(title = "All") +
-	theme_mine_legend_bottom())
+                                         aes(x = Result, y = Runs, fill = Runs_class)) +
+                                    geom_bar(stat = "identity", width = 1) + labs(title = "All") +
+                                    theme_mine_legend_bottom())
 
 
 overall_extras_all <-
@@ -128,7 +137,7 @@ overall_extras_lost <-
 
 
 overall_extras_all_pie <- ggplot(overall_extras_all,
-       aes(x = Result, y = Runs, fill = Extras_class)) +
+                                 aes(x = Result, y = Runs, fill = Extras_class)) +
   geom_bar(stat = "identity", width = 1) + labs(title = "All") +
   theme_mine_no_legend() + coord_polar(theta = "y")  +
   geom_text(aes(x= 1.2, y = Runs / 2 + c(0, cumsum(Runs)[-length(Runs)]),
@@ -136,7 +145,7 @@ overall_extras_all_pie <- ggplot(overall_extras_all,
   theme(plot.title = element_text(size = 13, face = "bold", margin = margin(b = -5, unit = "pt")))
 
 overall_extras_won_pie <- ggplot(overall_extras_won,
-       aes(x = Result, y = Runs, fill = Extras_class)) +
+                                 aes(x = Result, y = Runs, fill = Extras_class)) +
   geom_bar(stat = "identity", width = 1) + labs(title = "Won") +
   theme_mine_no_legend() + coord_polar(theta = "y")  +
   geom_text(aes(x= 1.2, y = Runs / 2 + c(0, cumsum(Runs)[-length(Runs)]),
@@ -144,7 +153,7 @@ overall_extras_won_pie <- ggplot(overall_extras_won,
   theme(plot.title = element_text(size = 13, face = "bold", margin = margin(b = -5, unit = "pt")))
 
 overall_extras_lost_pie <- ggplot(overall_extras_lost,
-       aes(x = Result, y = Runs, fill = Extras_class)) +
+                                  aes(x = Result, y = Runs, fill = Extras_class)) +
   geom_bar(stat = "identity", width = 1) + labs(title = "Lost") +
   theme_mine_no_legend() + coord_polar(theta = "y")  +
   geom_text(aes(x= 1.2, y = Runs / 2 + c(0, cumsum(Runs)[-length(Runs)]),
@@ -153,22 +162,22 @@ overall_extras_lost_pie <- ggplot(overall_extras_lost,
 
 
 overall_extras_comb_pie <-
-		grid.arrange(overall_extras_all_pie, overall_extras_won_pie,
-			overall_extras_lost_pie, top = textGrob("Extras received by the Teams",
-				gp=gpar(fontsize=18,font_family = "helvetica", fontface = "bold"), vjust=4.25),
-			bottom = textGrob("Composition of extras by category(in %)",
-			                  gp=gpar(fontsize=15,font_family = "helvetica"), vjust=-17.65),
-			nrow = 1, ncol = 3)
+  grid.arrange(overall_extras_all_pie, overall_extras_won_pie,
+               overall_extras_lost_pie, top = textGrob("Extras received by the Teams",
+                                                       gp=gpar(fontsize=18,font_family = "helvetica", fontface = "bold"), vjust=4.25),
+               bottom = textGrob("Composition of extras by category(in %)",
+                                 gp=gpar(fontsize=15,font_family = "helvetica"), vjust=-17.65),
+               nrow = 1, ncol = 3)
 
 
 legend_extras <- g_legend(ggplot(overall_extras_lost,
-       aes(x = Result, y = Runs, fill = Extras_class)) +
-  geom_bar(stat = "identity", width = 1) +
-	theme_mine_legend_bottom())
+                                 aes(x = Result, y = Runs, fill = Extras_class)) +
+                            geom_bar(stat = "identity", width = 1) +
+                            theme_mine_legend_bottom())
 
 #team_total_matchwise
 team_total_matchwise_ipl <- team_total_matchwise_ipl %>%
-                              mutate(`Runs from boundaries` = ((`4s`*4)+(`6s`*6)))
+  mutate(`Runs from boundaries` = ((`4s`*4)+(`6s`*6)))
 
 match_aggregates_ipl <- batting_ipl %>%
   inner_join(match_and_score_details_ipl, by = c("match_id", "team"="Batting Team")) %>%
@@ -186,23 +195,23 @@ match_aggregates_ipl <- batting_ipl %>%
             `Boundaries %` = round(((`6s`*6)+(`4s`*4))*100/Runs, digits = 2)
   ) %>% arrange(`Match_no`) %>%
   select( `Match` = `match`,
-         `Individual Innings`, `Runs`, `Balls`, `Not out`, `Highest`,
-         `Average`, `SR`, `4s`, `6s`, `Boundaries %`,
-         `Match_date` = `match_date`, `Match_no`,
-         `Team1` = `team1`, `Team2` = team2, `Winner` = winner,
-         `Match_Result` = match_result) %>%
+          `Individual Innings`, `Runs`, `Balls`, `Not out`, `Highest`,
+          `Average`, `SR`, `4s`, `6s`, `Boundaries %`,
+          `Match_date` = `match_date`, `Match_no`,
+          `Team1` = `team1`, `Team2` = team2, `Winner` = winner,
+          `Match_Result` = match_result) %>%
   mutate(`Runs from boundaries` = ((`4s`*4)+(`6s`*6))) %>%
   mutate(`1s, 2s and 3s` = Runs - `Runs from boundaries`)
 
 match_aggregates_1_ipl<- gather(match_aggregates_ipl,
-       key = `Runs_class`,
-       value = `Total`, `Runs from boundaries`, `1s, 2s and 3s`,
-       -c(`Match`,
-          `Individual Innings`, `Runs`, `Balls`, `Not out`, `Highest`,
-          `Average`, `SR`, `4s`, `6s`, `Boundaries %`,
-          `Match_date`, `Match_no`,
-          `Team1`, `Team2`, `Winner`,
-          `Match_Result`)) %>%
+                                key = `Runs_class`,
+                                value = `Total`, `Runs from boundaries`, `1s, 2s and 3s`,
+                                -c(`Match`,
+                                   `Individual Innings`, `Runs`, `Balls`, `Not out`, `Highest`,
+                                   `Average`, `SR`, `4s`, `6s`, `Boundaries %`,
+                                   `Match_date`, `Match_no`,
+                                   `Team1`, `Team2`, `Winner`,
+                                   `Match_Result`)) %>%
   mutate(Runs_class = factor(Runs_class, levels = c("Runs from boundaries", 
                                                     "1s, 2s and 3s")) )
 
@@ -210,18 +219,18 @@ match_aggregates_1_ipl<- gather(match_aggregates_ipl,
 
 
 all_total_ipl_bar <- ggplot(team_total_matchwise_ipl,
-       aes(
-         x = `Match_no`,
-         y = Runs,
-         fill = `Innings`,  frame = `Match_no`, 
-         cumulative = TRUE,
-         text = paste(`Match`, "\n", `Team`))) +
+                            aes(
+                              x = `Match_no`,
+                              y = Runs,
+                              fill = `Innings`,  frame = `Match_no`, 
+                              cumulative = TRUE,
+                              text = paste(`Match`, "\n", `Team`))) +
   geom_bar_interactive(
     position = "dodge",
     stat = "identity", width = 1) +
-    ggtitle("Total for every innings") + xlab("Match_no") + ylab("Runs") +
-    theme_new()+
-    scale_fill_economist()  
+  ggtitle("Total for every innings") + xlab("Match_no") + ylab("Runs") +
+  theme_new()+
+  scale_fill_economist()  
 
 ggsave("graphs/all_total_ipl_bar.svg", 
        all_total_ipl_bar, width = 16, height = 6)
@@ -231,28 +240,28 @@ gg_animate(all_total_ipl_bar,"graphs/all_total_ipl_bar.gif", interval = .01 ,
 
 
 all_aggregate_ipl_bar <- ggplot(data= match_aggregates_ipl,
-                        aes(x = `Match_no`,
-    y = Runs,text = paste(`Match`, ":", `Team1`, "vs", Team2, Winner, Match_Result, sep = " "))) +
+                                aes(x = `Match_no`,
+                                    y = Runs,text = paste(`Match`, ":", `Team1`, "vs", Team2, Winner, Match_Result, sep = " "))) +
   geom_bar( stat = "identity", color = "coral4",
             fill = "coral") +
   geom_point(aes(x=`Match_no`, y=`Runs from boundaries`,
                  fill = "Runs from boundaries"),
              shape = 45, size =15,
-              colour = "coral4")+
+             colour = "coral4")+
   # ggtitle("Match Aggregates", sub = "Runs made in every match") +
   # xlab("Match_no") + ylab("Runs") +
-#  theme_wsj()
-+ labs(x="Match_no", y="Runs", title="Match Aggregates",
-      subtitle="Runs made in every match")+
+  #  theme_wsj()
+  + labs(x="Match_no", y="Runs", title="Match Aggregates",
+         subtitle="Runs made in every match")+
   theme(legend.title=element_blank(),
         legend.justification=c(.47,-.5),
         legend.position=c(.47,-.5))+
   theme(plot.subtitle=element_text(size=16, hjust=0.5,
                                    face="italic", color="black"))
-  # scale_color_manual(name = element_blank(),
-  #                    labels = "Runs from boundaries",
-  #                    values = "coral4")+
-  # scale_shape_manual(values = 45)+ theme(axis.line=element_blank(),axis.text.x=element_blank(),
+# scale_color_manual(name = element_blank(),
+#                    labels = "Runs from boundaries",
+#                    values = "coral4")+
+# scale_shape_manual(values = 45)+ theme(axis.line=element_blank(),axis.text.x=element_blank(),
 + theme(axis.line=element_blank(),axis.text.x=element_blank(),
         axis.text.y=element_blank(),axis.ticks=element_blank(),
         axis.title.x=element_blank(),
@@ -268,14 +277,20 @@ all_aggregate_ipl_bar <- ggplot(data= match_aggregates_1_ipl,
                                     y = Total,
                                     fill = Runs_class,
                                     tooltip = paste(`Match`, ":", `Team1`, 
-                                                 "vs", Team2, Winner, 
-                                                 Match_Result, sep = " "))) +
+                                                    "vs", Team2, "\n", Winner, 
+                                                    Match_Result, sep = " "))) +
   geom_bar_interactive( stat = "identity" ) +
   ggtitle("Total for every innings") + xlab("Match_no") + ylab("Runs") +
   theme_new()+
-  scale_fill_economist()  
+  scale_fill_economist() 
 
-ggiraph(code = print(all_aggregate_ipl_bar), width_svg = 16,
+ggsave("graphs/all_total_ipl_bar.svg", 
+       all_total_ipl_bar, width = 16, height = 6)
+
+ggsave("graphs/all_total_ipl_bar_new.svg", 
+       all_total_ipl_bar)
+
+ggiraph(code = print(all_aggregate_ipl_bar), width_svg = 26,
         height_svg = 6)
 
 match_aggregates_1_ipl %>% group_by(`Match_no`,`Runs_class` ) %>%
@@ -300,23 +315,23 @@ ggplot(data= match_aggregates_1_ipl,
         plot.margin = unit(c(10,5,25,5),"mm"),
         plot.background = element_blank(),
         panel.background = element_blank()) + theme_new()+
-    scale_fill_economist()
+  scale_fill_economist()
 
 boundaries_percentage_per_match_line <- ggplot(team_total_matchwise_ipl)+
-           geom_line(aes(x=`Match_no`, y=`Boundaries %`), color = "brown2")+
-           geom_hline(yintercept = 60.67, color = "brown4")+
-           annotate("text", x = 57, y = 80, label = "Overall: 60.67 %")+
-           xlab("Match") + ylab("Boundaries Percentage") +
-           ggtitle("Percentage of Runs in Boundaries per Match")+ theme_mine()+
-           annotate("segment", x = 59, xend = 59, y = 78,  yend = 62,
-                    colour="brown4", size=0.4, arrow=arrow(length=unit(.2, "cm")))
+  geom_line(aes(x=`Match_no`, y=`Boundaries %`), color = "brown2")+
+  geom_hline(yintercept = 60.67, color = "brown4")+
+  annotate("text", x = 57, y = 80, label = "Overall: 60.67 %")+
+  xlab("Match") + ylab("Boundaries Percentage") +
+  ggtitle("Percentage of Runs in Boundaries per Match")+ theme_mine()+
+  annotate("segment", x = 59, xend = 59, y = 78,  yend = 62,
+           colour="brown4", size=0.4, arrow=arrow(length=unit(.2, "cm")))
 
 highest_score_in_every_match <- ggplot(team_total_matchwise_ipl)+
-           geom_line(aes(x=`Match_no`, y=`Highest`), color = "brown2")+
-           geom_line(aes(x=`Match_no`, y=((`4s`*4)+(`6s`*6)), color = "yellow"))+
-           geom_hline(yintercept = 133, color = "brown4")+ ylim(0,160)+
-           annotate("text", x = 57, y = 153, label = "Overall highest: 133")+
-           xlab("Match") + ylab("Runs") +
-           ggtitle("Highest score and Runs in Boundaries in every Match")+ theme_mine()+
-           annotate("segment", x = 59, xend = 59, y = 150,  yend = 135,
-                    colour="brown4", size=0.4, arrow=arrow(length=unit(.2, "cm")))
+  geom_line(aes(x=`Match_no`, y=`Highest`), color = "brown2")+
+  geom_line(aes(x=`Match_no`, y=((`4s`*4)+(`6s`*6)), color = "yellow"))+
+  geom_hline(yintercept = 133, color = "brown4")+ ylim(0,160)+
+  annotate("text", x = 57, y = 153, label = "Overall highest: 133")+
+  xlab("Match") + ylab("Runs") +
+  ggtitle("Highest score and Runs in Boundaries in every Match")+ theme_mine()+
+  annotate("segment", x = 59, xend = 59, y = 150,  yend = 135,
+           colour="brown4", size=0.4, arrow=arrow(length=unit(.2, "cm")))
